a16f42a2623b46b0fde04aeead50db2a40fbab4f
BrandonYahir <byahir722@gmail.com>
2021-10-04 20:01:23 -0500

    Se agrega el proyecto

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <div id="header">
+      <Search v-on:query-change="querySearch" />  
+    </div>
+
+      <div id="main-container">
+        <h2>Tareas</h2>
+        <TodoAdd v-on:add-todo="addTodo" />
+        <Todos v-bind:todoslist="copyTodos" v-on:delete-todo="deleteTodo"/>
+      </div> 
+      <misDatos nombre= 'Brandon Yahir Flores Vazquez'/>
+  </div>
+
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue'
+import TodoAdd from './components/TodoAdd.vue';
+import Todos from './components/Todos.vue';
+import misDatos from './components/misDatos.vue';
+
+export default {  
+  name: 'App',
+  components: {
+    Todos, TodoAdd, Search, misDatos
+  },
+
+  methods: {
+    deleteTodo(id){
+      //console.log(id);
+      this.todos = this.todos.filter(todo => todo.id != id);
+      this.copyTodos = [ ... this.todos];
+    },
+
+    addTodo (todo) {
+      this.todos.push(todo);
+      this.copyTodos  = [ ... this.todos];
+    },
+
+    querySearch (query){
+      if (query.trim() === '') {
+        this.copyTodos = [ ... this.todos];
+      } else {
+        const temp = this.todos.filter(todo => {
+          return todo.title.includes(query)
+        });
+
+        this.copyTodos = [ ... temp]
+      }
+    }
+  },
+
+  data () {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: 'Formatear Equipo de Computo',
+          completed: false
+        },
+        {
+          id: 1,
+          title: 'Darle de comer a Pinky',
+          completed: true
+        },
+        {
+          id: 2,
+          title: 'Cenar con Marry',
+          completed: false
+        },        {
+          id: 3,
+          title: 'Terminar aplicacion',
+          completed: true
+        }
+      ],
+
+      copyTodos: []
+    }
+  },
+
+  created () {
+    this.copyTodos = [ ... this.todos]
+  }
+}
+</script>
+
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;    
+  }
+
+  #main-container {
+    border: solid 1px #ccc;
+    width: 600px;
+    margin: 100px auto;
+  }
+
+  #header {
+    background: black;
+    padding: 10px;
+  }
+
+  h2 {
+    padding: 0 10px;
+  }
+
+</style>
---
+++ src/components/misDatos.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="footer">
+    <footer>
+        {{ nombre }} <br/>
+      Grupo: {{ Grupo }}<br/>
+        Materia: {{ Materia }}<br/>
+      Carrera: {{ Carrera }}
+    </footer>
+</div>
+</template>
+
+<script>
+export default {
+  name: "misDatos",
+  props: {
+    nombre: String,
+  },
+  data() {
+    return {
+      Grupo: '4° A',
+      Materia: 'Aplicaciones Web Orientadas a Servicios.',
+      Carrera: 'Tecnologias de la Informacion Desarrollo de Software Multiplataforma.'
+    };
+  },
+};
+</script>
+
+<style scoped>
+.footer{
+    margin: 20px 0px 20px 0;
+    background-color: yellow;
+    color: rgb(255, 10, 10);
+    text-align: center;
+}
+</style>
